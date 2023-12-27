336b431f206553a3ed69c02e92da32e4aed4e34c
Nabeel77 <nabeel.beeni30@gmail.com>
2023-12-27 16:00:49 +0200

    Added matchers for listener

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 pub struct Server {
     address: String,
 }
@@ -16,7 +17,18 @@ impl Server {
         let listener = TcpListener::bind(&self.address).unwrap();
         // infinite loop syntax
         loop {
-            listener.accept();
+            match listener.accept() {
+                Ok((mut stream, address)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {:?}", String::from_utf8_lossy(&buffer))
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e)
+            }
         }
     }
 }
--- target/debug/deps/http_server-d22243846d6be93c.d
+++ target/debug/deps/http_server-d22243846d6be93c.d
@@ -1,5 +1,9 @@
-/Users/nabeel.77.dev/Desktop/activities_rust/http_server/target/debug/deps/http_server-d22243846d6be93c: src/main.rs
+/Users/nabeel.77.dev/Desktop/activities_rust/http_server/target/debug/deps/http_server-d22243846d6be93c: src/main.rs src/http/mod.rs src/http/request.rs src/http/method.rs src/server.rs
 
-/Users/nabeel.77.dev/Desktop/activities_rust/http_server/target/debug/deps/http_server-d22243846d6be93c.d: src/main.rs
+/Users/nabeel.77.dev/Desktop/activities_rust/http_server/target/debug/deps/http_server-d22243846d6be93c.d: src/main.rs src/http/mod.rs src/http/request.rs src/http/method.rs src/server.rs
 
 src/main.rs:
+src/http/mod.rs:
+src/http/request.rs:
+src/http/method.rs:
+src/server.rs:
