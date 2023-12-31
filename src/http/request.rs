847dcb1b07a4b9aa5c176442017069f7909b0d42
Nabeel77 <nabeel.beeni30@gmail.com>
2023-12-31 16:16:02 +0200

    Added pattern matching for query path

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,6 @@
 use std::str;
 use std::str::Utf8Error;
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Result as FmtResult, Display, Debug, Formatter};
@@ -17,10 +17,16 @@ impl TryFrom<&[u8]> for Request {
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
+        }
+        let method: Method = method.parse()?;
+        let mut query_string = None;
+        if let Some(i) = path.find("?") {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
         }
         unimplemented!()
     }
@@ -69,6 +75,12 @@ impl Debug for ParseError {
 impl From<Utf8Error> for ParseError {
     fn from(_: Utf8Error) -> Self {
         Self::InvalidEncoding
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
     }
 }
 
